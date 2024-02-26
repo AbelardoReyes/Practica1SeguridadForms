f8f6133590e4a38a6e99de82ba76e485d39fc975
Abelardo Garcia Reyes <abelardoreyes256@gmail.com>
2024-02-25 22:20:59 -0600

    [UPD] fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manifest: true,
+            },
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
